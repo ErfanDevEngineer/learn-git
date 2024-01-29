7fa0c0607e05457ad858a41783e09d587b2a6759
Erfan Kazemi <erfankazemy1380@gmail.com>
2024-01-29 14:18:11 +0330

    add message feature

--- js/funcs.js
+++ js/funcs.js
@@ -10,3 +10,8 @@ const isLoggin = ()=>{
 const usersInfo = [
     {id:1,userName:"Erfan KAzemi" , job:"WEb Dwveloper"}
 ]
+
+const message = [
+    {id:1,userName:"Erfan Kazemi" , msg:"yur products are asesome,... congratulation to you!"},
+    {id:2,userName:"Erfan Kazemi" , msg:"you are always get alone"}
+]
