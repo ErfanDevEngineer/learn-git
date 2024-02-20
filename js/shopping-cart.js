c60b8724b8fc541cf713f49317484dd1740c926b
ErfanDevEngineer <159643139+ErfanDevEngineer@users.noreply.github.com>
2024-02-20 17:18:23 +0000

    add new obj in shopping cart

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,5 +1,6 @@
 const basket = [
     {id:1, product:"laptop" , price:60_000_000},
     {id:2, product:"shiomi m13" , price:12_000_000},
-    {id:3, product:"mackbook" , price:120_000_000}
+    {id:3, product:"mackbook" , price:120_000_000},
+    {id:4, product:"206 pegeout" , price:360_000_000}
 ]
